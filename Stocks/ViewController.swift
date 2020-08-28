d146abe9ad6739c6ed34338a3d087efe4785fef5
Igor Silaev <cokturqi55@mail.ru>
2020-08-28 23:06:06 +0300

    Base ready

--- Stocks/ViewController.swift
+++ Stocks/ViewController.swift
@@ -9,12 +9,149 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var companyNameLabel: UILabel!
+    @IBOutlet weak var companyPickerView: UIPickerView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var companySymbolLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var priceChangeLabel: UILabel!
+    
+    // MARK: - Private properties
+    private lazy var companies = [
+        "Apple" : "AAPL",
+        "Microsoft" : "MSFT",
+        "Google" : "GOOG",
+        "Amazon" : "AMZN",
+        "Facebook" : "FB"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        companyPickerView.dataSource = self
+        companyPickerView.delegate = self
+        
+        activityIndicator.hidesWhenStopped = true
+        
+        requestQuoteUpdate()
     }
-
-
+    
+    // MARK: - Private methods
+    
+    private func requestQuote(for symbol: String) {
+        let token = "pk_4b1273e91976462098febe7ae10e4a9f"
+        guard let url = URL(string: "https://cloud.iexapis.com/stable/stock/\(symbol)/quote?token=\(token)") else {
+            return
+        }
+        
+        let dataTask = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let data = data,
+                (response as? HTTPURLResponse)?.statusCode == 200,
+                error == nil {
+                self.parseQuote(from: data)
+            } else {
+                print("Network error")
+            }
+        }
+        dataTask.resume()
+    }
+    
+    private func requestCompaniesList() {
+        let token = "pk_4b1273e91976462098febe7ae10e4a9f"
+        guard let url = URL(string: "https://cloud.iexapis.com/stable/stock/market/list/infocus?token=\(token)") else {
+            return
+        }
+        
+        let dataTask = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let data = data,
+                (response as? HTTPURLResponse)?.statusCode == 200,
+                error == nil {
+                self.parseQuote(from: data)
+            } else {
+                print("Network error")
+            }
+        }
+        dataTask.resume()
+    }
+    
+    private func parseQuote(from data: Data) {
+        do {
+            let jsonObject = try JSONSerialization.jsonObject(with: data)
+            
+            guard
+                let json = jsonObject as? [String: Any],
+                let companyName = json["companyName"] as? String,
+                let companySymbol = json["symbol"] as? String,
+                let price = json["latestPrice"] as? Double,
+                let priceChange = json["change"] as? Double else { return print("Invalid JSON") }
+            
+            DispatchQueue.main.async {
+                self.displayStockInfo(companyName: companyName,
+                                      companySymbol: companySymbol,
+                                      price: price,
+                                      priceChange: priceChange)
+            }
+        } catch {
+            print("JSON parsing error: " + error.localizedDescription)
+        }
+    }
+    
+    private func displayStockInfo(companyName: String,
+                                  companySymbol: String,
+                                  price: Double,
+                                  priceChange: Double) {
+        activityIndicator.stopAnimating()
+        companyNameLabel.text = companyName
+        companySymbolLabel.text = companySymbol
+        priceLabel.text = "\(price)"
+        priceChangeLabel.text = "\(priceChange)"
+        if priceChange < 0 {
+            priceChangeLabel.textColor = .red
+        } else if priceChange > 0 {
+            priceChangeLabel.textColor = .green
+        } else {
+            priceChangeLabel.textColor = .black
+        }
+    }
+    
+    private func requestQuoteUpdate() {
+        activityIndicator.startAnimating()
+        companyNameLabel.text = "-"
+        companySymbolLabel.text = "-"
+        priceLabel.text = "-"
+        priceChangeLabel.textColor = .black
+        priceChangeLabel.text = "-"
+        
+        let selectedRow = companyPickerView.selectedRow(inComponent: 0)
+        let selectedSymbol = Array(companies.values)[selectedRow]
+        requestQuote(for: selectedSymbol)
+    }
+    
 }
 
+// MARK: - UIPickerViewDataSource
+
+extension ViewController: UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return companies.keys.count
+    }
+}
+
+// MARK: - UIPickerViewDelegate
+
+extension ViewController: UIPickerViewDelegate {
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return Array(companies.keys)[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        requestQuoteUpdate()
+    }
+}
