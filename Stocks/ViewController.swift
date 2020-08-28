2c1f59807d484594543c529dba89f3c1684b0f44
Igor Silaev <cokturqi55@mail.ru>
2020-08-28 19:02:44 +0300

    Initial Commit

---
+++ Stocks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stocks
+//
+//  Created by Игорь Силаев on 28.08.2020.
+//  Copyright © 2020 Игорь Силаев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
